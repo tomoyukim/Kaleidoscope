1a99b20ed0512358ea6a7654f5cd7b9f12422b78
Gergely Nagy <algernon@keyboard.io>
2019-10-24 17:10:49 +0200

    hardware::Planck: Merged the KeyScannerDescription into the main header
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- src/kaleidoscope/hardware/olkb/Planck.h
+++ src/kaleidoscope/hardware/olkb/Planck.h
@@ -22,8 +22,7 @@
 #include <Arduino.h>
 #define HARDWARE_IMPLEMENTATION kaleidoscope::hardware::olkb::Planck
 
-#include "kaleidoscope/hardware/olkb/planck/PlanckKeyScannerDescription.h"
-
+#include "kaleidoscope/driver/keyscanner/ATMegaKeyScannerDescription.h"
 #include "kaleidoscope/driver/keyscanner/ATMegaKeyScanner.h"
 #include "kaleidoscope/driver/bootloader/avr/HalfKay.h"
 #include "kaleidoscope/hardware/avr/AVRDeviceDescription.h"
@@ -34,8 +33,13 @@ namespace hardware {
 namespace olkb {
 
 struct PlanckDeviceDescription : kaleidoscope::hardware::avr::AVRDeviceDescription {
-  typedef PlanckKeyScannerDescription KeyScannerDescription;
-  typedef kaleidoscope::driver::keyscanner::ATMegaKeyScanner<PlanckKeyScannerDescription> KeyScanner;
+  typedef struct PlanckKeyScannerDescription : public kaleidoscope::driver::keyscanner::ATMegaKeyScannerDescription {
+    ATMEGA_KEYSCANNER_DESCRIPTION(
+      ROW_PIN_LIST({ PIN_D0, PIN_D5, PIN_B5, PIN_B6 }),
+      COL_PIN_LIST({ PIN_F1, PIN_F0, PIN_B0, PIN_C7, PIN_F4, PIN_F5, PIN_F6, PIN_F7, PIN_D4, PIN_D6, PIN_B4, PIN_D7 })
+    );
+  } KeyScannerDescription;
+  typedef kaleidoscope::driver::keyscanner::ATMegaKeyScanner<KeyScannerDescription> KeyScanner;
   typedef kaleidoscope::driver::bootloader::avr::HalfKay BootLoader;
 };
 
