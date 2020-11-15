06cfc43a720a81bc8ccc3fcc8b1d584ba7cf564e
Jesse Vincent <jesse@keyboard.io>
2020-11-14 18:05:59 -0800

    Start to lift code to the makefile

--- etc/sketch-arduino-cli.mk
+++ etc/sketch-arduino-cli.mk
@@ -1,7 +1,18 @@
 mkfile_path := $(abspath $(lastword $(MAKEFILE_LIST)))
 mkfile_dir := $(dir $(mkfile_path))
 
-KALEIDOSCOPE_BUILDER_DIR ?= $(abspath $(mkfile_dir)/../bin)
+
+export KALEIDOSCOPE_DIR ?= $(abspath $(mkfile_dir)/..)
+export KALEIDOSCOPE_BIN_DIR ?= $(KALEIDOSCOPE_DIR)/bin
+export ARDUINO_CONTENT ?= $(KALEIDOSCOPE_DIR)/.arduino
+export ARDUINO_DIRECTORIES_DATA ?= $(ARDUINO_CONTENT)/data
+export ARDUINO_DIRECTORIES_DOWNLOADS ?= $(ARDUINO_CONTENT)/downloads
+export ARDUINO_DIRECTORIES_USER ?= $(ARDUINO_CONTENT)/user
+export ARDUINO_CLI_CONFIG ?= $(ARDUINO_DIRECTORIES_DATA)/arduino-cli.yaml
+export ARDUINO_BOARDS_MANAGER_KALEIDOSCOPE ?= https://raw.githubusercontent.com/keyboardio/boardsmanager/master/package_keyboardio_index.json
+
+
+
 
 .DEFAULT_GOAL := compile
 
@@ -20,5 +31,5 @@ flash: compile
 
 
 %:
-	ARDUINO_DIRECTORIES_USER=$(ARDUINO_DIRECTORIES_USER) $(KALEIDOSCOPE_BUILDER_DIR)/kaleidoscope-builder $@
+	ARDUINO_DIRECTORIES_USER=$(ARDUINO_DIRECTORIES_USER) $(KALEIDOSCOPE_BIN_DIR)/kaleidoscope-builder $@
 
